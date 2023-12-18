2ae5413dc9e55c81159aa343fac134f0655efd2e
Adithya Ajith <aajith@kantime.com>
2023-12-18 11:43:31 -0600

    Create UI for roshambo and ensure functionality

--- roshambo.js
+++ roshambo.js
@@ -9,10 +9,67 @@ console.log("Hello, world");
 // If x === (y - 1) % 3, x wins (using mathematical modulo and not remainder)
 // Else, y wins
 
-function mod(n, m) {
-  return ((n % m) + m) % m;
+const ROCK_CHOICE = 1;
+const PAPER_CHOICE = 0;
+const SCISSORS_CHOICE = 2;
+
+const PLAYER_SCOREBOARD = document.querySelector('.player-score-text');
+const COMPUTER_SCOREBOARD = document.querySelector('.computer-score-text');
+const ANNOUNCER = document.querySelector('.announcer-text');
+const RESTART_BUTTON = document.querySelector('#reset-button');
+
+const PICK_ROCK_BUTTON = document.querySelector("#play-rock");
+const PICK_PAPER_BUTTON = document.querySelector("#play-paper");
+const PICK_SCISSORS_BUTTON = document.querySelector("#play-scissors");
+
+RESTART_BUTTON.addEventListener("click", restartGame);
+
+let playerScore = 0;
+let computerScore = 0;
+
+function updateScoreText() {
+    PLAYER_SCOREBOARD.textContent = playerScore;
+    COMPUTER_SCOREBOARD.textContent = computerScore;
 }
 
+function disableButtons(disabled) {
+    PICK_PAPER_BUTTON.disabled = disabled;
+    PICK_ROCK_BUTTON.disabled = disabled;
+    PICK_SCISSORS_BUTTON.disabled = disabled;
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    ANNOUNCER.textContent = `Choose an option!`;
+    updateScoreText();
+    disableButtons(false);
+}
+
+function gameOver() {
+    if (playerScore > computerScore) {
+        ANNOUNCER.textContent = "You won! Press restart to play again.";
+        if (confirm(`You have beaten the computer! Play again?`)) {
+            restartGame();
+            return;
+        }
+        disableButtons(true);
+    }
+    else {
+        ANNOUNCER.textContent = "You lost! Press restart to play again.";
+        if (confirm(`The computer has beaten you! Play again?`)) {
+            restartGame();
+            return;
+        }
+        disableButtons(true);
+    }
+}
+
+function mod(n, m) {
+    return ((n % m) + m) % m;
+}
+
+// Better off creating a 2-way dictionary for the two below methods
 function translateChoiceToNum(choice) {
     switch (choice.toLowerCase()) {
         case "paper":
@@ -40,53 +97,25 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(`You have chosen ${translateNumToChoice(playerSelection)}. \n The computer has chosen ${translateNumToChoice(computerSelection)}.`)
+    ANNOUNCER.textContent = `You have chosen ${translateNumToChoice(playerSelection)}. \n The computer has chosen ${translateNumToChoice(computerSelection)}. `;
     if (playerSelection === computerSelection) {
-        return 0; // TIE GAME
-    }
-    if (playerSelection === mod(computerSelection - 1, 3)) {
-        return 1; // Player wins
-    }
-    else {
-        return 2; // Player loses
-    }
-}
-
-console.log(playRound(2, 0));
-
-function game() {
-
-    let playerScore = 0;
-    let computerScore = 0;
-    while (playerScore < 3 && computerScore < 3) {
-        let playerChoiceText = prompt('Choose rock, paper, or scissors!');
-        let playerChoiceNum = translateChoiceToNum(playerChoiceText);
-    
-        let computerChoiceNum = getComputerChoice();
-
-        let result = playRound(playerChoiceNum, computerChoiceNum);
-        switch (result) {
-            case 0:
-                console.log("Tie round! Go again!");
-                continue;
-            case 1:
-                console.log(`You win this round! ${translateNumToChoice(playerChoiceNum)} beats ${translateNumToChoice(computerChoiceNum)}.`);
-                playerScore++;
-                break;
-            default:
-                console.log(`You lose this round! ${translateNumToChoice(computerChoiceNum)} beats ${translateNumToChoice(playerChoiceNum)}.`);
-                computerScore++;
-                break;
-
-        }
-    }
-
-    console.log(`Final score! \n You: ${playerScore} \n Computer: ${computerScore}`)
-    if (playerScore > computerScore) {
-        console.log("You have beaten the computer!");
+        ANNOUNCER.textContent += "Tie round! Play again!"
         return;
     }
-    console.log("The computer has beaten you!");
+    if (playerSelection === mod(computerSelection - 1, 3)) {
+        ANNOUNCER.textContent += "You win this round!"
+        playerScore++;
+    }
+    else {
+        ANNOUNCER.textContent += "The computer wins this round!"
+        computerScore++;
+    }
+    updateScoreText();
+    if (playerScore >= 3 || computerScore >= 3) {
+        gameOver();
+    }
 }
 
-game();
+PICK_ROCK_BUTTON.addEventListener("click", function () { playRound(ROCK_CHOICE, getComputerChoice()); });
+PICK_PAPER_BUTTON.addEventListener("click", function () { playRound(PAPER_CHOICE, getComputerChoice()); });
+PICK_SCISSORS_BUTTON.addEventListener("click", function () { playRound(SCISSORS_CHOICE, getComputerChoice()); });
